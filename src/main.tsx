10a471fbe93c36fc3aa00db15a046e336a6c9912
tui2tone <tui2tone@gmail.com>
2022-10-29 22:49:05 +0700

    :sparkles: change to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { DialogProvider } from './components/GlobalContext/useDialog'
 import { ConfirmModalProvider } from './components/ModalWithContext/useConfirmModal'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DialogProvider>
         <ConfirmModalProvider>
           <App />
         </ConfirmModalProvider>
       </DialogProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
